9d216dcc8181950f5a9fd24d67cc74c9a4c94743
shahanakhundmir <58731479+shahanakhundmir@users.noreply.github.com>
2021-05-16 14:00:09 +0100

    added recursion kata Greatest common divisor

--- exclamation.py
+++
@@ -1,8 +0,0 @@
-def remove_exclamation_marks(s):
-    print(s)
-    #filter all chars not equal to !
-    t = "".join(filter(lambda x: x!= "!", s))
-    return(t)
-    #return filter string
-
-    #return "ggg"
